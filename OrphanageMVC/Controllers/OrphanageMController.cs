ebfbd9a94a046ac0dc9e95984d39fa68b9df43fe
Rajeev Chourey <rajeevchourey786@gmail.com>
2021-09-13 10:53:22 +0530

    updated views

--- OrphanageMVC/Controllers/OrphanageMController.cs
+++ OrphanageMVC/Controllers/OrphanageMController.cs
@@ -2,13 +2,10 @@
 using Newtonsoft.Json;
 using OrphanageMVC.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
-
+using BCrypt.Net;
 namespace OrphanageMVC.Controllers
 {
     public class OrphanageMController : Controller
@@ -65,7 +62,6 @@ namespace OrphanageMVC.Controllers
         public ActionResult Create(OrphanageRegistrationView orp)
         {
             HttpClient hc = new HttpClient();
-
             hc.BaseAddress = new Uri("http://localhost:64581/api/orphanage/register");
             var consumeapi = hc.PostAsJsonAsync("register", orp);
 
@@ -99,7 +95,7 @@ namespace OrphanageMVC.Controllers
                 FormsAuthentication.SetAuthCookie(orp.oId.ToString(), true);
                 
                 HttpClient hc = new HttpClient();
-
+                //orp.password = ps;
                 hc.BaseAddress = new Uri("http://localhost:64581/api/orphanage/login");
                 var consumeapi = hc.PostAsJsonAsync<OrphanageRegistrationView>("Login", orp);
 
