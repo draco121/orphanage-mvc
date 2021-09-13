4531386769258293a7ed36c2c6619b59c4904f83
PallaviVanarse <pallavivanarse27@gmail.com>
2021-09-13 10:34:59 +0530

    password encryption added

---
+++ OMVC/OrphanageMVC/Models/reqTable.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrphanageMVC.Models
+{
+    public class reqTable
+    {
+        public int rId { get; set; }
+        public string requirementName { get; set; }
+        public string requirementStatus { get; set; }
+        public int oId { get; set; }
+        [JsonIgnore]
+        public virtual OrphanageRegistrationView orphanageRegistration1 { get; set; }
+    }
+}
