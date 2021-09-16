f3b1cd6bdc6ab133ac7c7be0331316ed11af0b9e
Rajeev Chourey <rajeevchourey786@gmail.com>
2021-09-16 13:42:48 +0530

    c

--- OrphanageMVC/Common/Password.cs
+++ OrphanageMVC/Common/Password.cs
@@ -7,5 +7,19 @@ namespace OrphanageMVC.Common
 {
     public class Password
     {
+        public string Encode(string password)
+        {
+            try
+            {
+                byte[] EncDataByte = new byte[password.Length];
+                EncDataByte = System.Text.Encoding.UTF32.GetBytes(password);
+                string EncryptedData = Convert.ToBase64String(EncDataByte);
+                return EncryptedData;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in Encode" + ex.Message);
+            }
+        }
     }
 }
