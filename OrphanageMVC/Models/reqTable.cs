34fb13b7cfa3028af1c4153c517beb9f8c1ccb0c
Rajeev Chourey <rajeevchourey786@gmail.com>
2021-09-16 13:15:14 +0530

    c

---
+++ OrphanageMVC/Models/reqTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrphanageMVC.Models
+{
+    public class reqTable
+    {
+            public string Id { get; set; }
+            public int amount { get; set; }
+            public string description { get; set; }
+            public string status { get; set; }
+            public System.DateTime date { get; set; }
+            public int oId { get; set; }
+
+    }
+}
